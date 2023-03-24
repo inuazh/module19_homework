185ede7e1c2b6dbca5a54f5efa623496961b4257
Ramazan <inuazh.work@yandex.ru>
2023-03-24 20:50:22 +0300

    tvCommit

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
-class ElectricalDevice {
-    constructor(name) {
-      this.name = name;
-      this.powerOn = false;
-    }
-    
-    turnOn() {
-      if (!this.powerOn) {
-        console.log(`${this.name} включен`);
-        this.powerOn = true;
-      } else {
-        console.log(`${this.name} уже включен`);
-      }
-    }
-    
-    turnOff() {
-      if (this.powerOn) {
-        console.log(`${this.name} выключен`);
-        this.powerOn = false;
-      } else {
-        console.log(`${this.name} уже выключена`);
-      }
-    }
+
+function Lamp() {
+  this.name = "Лампа";
+  this.powerOn = false;
+}
+
+Lamp.prototype.turnOn = function() {
+  if (!this.powerOn) {
+    console.log(`${this.name} включена`);
+    this.powerOn = true;
+  } else {
+    console.log(`${this.name} уже включена`);
   }
-  
-  const myLamp = new ElectricalDevice('Лампа');
-  myLamp.turnOn(); // Лампа включен
-  myLamp.turnOff(); // Лампа выключен
-  myLamp.turnOff(); // Лампа уже выключена
+}
+
+Lamp.prototype.turnOff = function() {
+  if (this.powerOn) {
+    console.log(`${this.name} выключена`);
+    this.powerOn = false;
+  } else {
+    console.log(`${this.name} уже выключена`);
+  }
+}
+
+
+function TV() {
+  this.name = "Телевизор";
+  this.powerOn = false;
+}
+
+TV.prototype.turnOn = function() {
+  if (!this.powerOn) {
+    console.log(`${this.name} включен`);
+    this.powerOn = true;
+  } else {
+    console.log(`${this.name} уже включен`);
+  }
+}
+
+TV.prototype.turnOff = function() {
+  if (this.powerOn) {
+    console.log(`${this.name} выключен`);
+    this.powerOn = false;
+  } else {
+    console.log(`${this.name} уже выключен`);
+  }
+}
+
+
+const myLamp = new Lamp();
+
+
+const myTV = new TV();
+
+// Создание делегирующей связи [[Prototype]] между лампой и телевизором
+Object.setPrototypeOf(myTV, myLamp);
+
+
+myTV.turnOn(); // Телевизор включен
+myTV.turnOff(); // Телевизор выключен
+myTV.turnOff(); // Телевизор уже выключен
+
   
