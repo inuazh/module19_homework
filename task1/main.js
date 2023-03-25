52fbf2ec84d0cb4f9aefebc359c209e66f01d45d
Ramazan <inuazh.work@yandex.ru>
2023-03-25 18:31:17 +0300

    task1done

--- task1/main.js
+++ task1/main.js
@@ -1,2 +1,19 @@
+function printOwnProp(obj) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            console.log(`${key}: ${obj[key]}`);
+        }
+    }
+}
 
   
+
+
+// for instance
+const obj = {
+    name: 'Milana',
+    age: 30,
+    city: 'Derbent'
+};
+
+printOwnProp(obj);
